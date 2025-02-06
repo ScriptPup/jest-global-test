e0a239ddf8bf63b84e801fe280af69d2727bde93
scriptpup <ethan_hammond@msn.com>
2025-02-06 18:36:50 -0500

    Fix async standups

--- TestGlobalSetup/tests/teardown-async.ts
+++ TestGlobalSetup/tests/teardown-async.ts
@@ -2,6 +2,6 @@ import { JestConfigWithTsJest } from "ts-jest";
 
 module.exports = async (globalConfig: JestConfigWithTsJest, projectConfig: JestConfigWithTsJest) => {    
     console.log("Starting global teardown");
-    await new Promise(() => { setTimeout(() => { }, 1000) });
+    await new Promise((res) => { setTimeout(() => { res(null); }, 1000) });
     console.log("Ending global teardown")
 }
